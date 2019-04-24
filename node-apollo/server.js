8d013cdcd508ad565cf84e8ca861224c91472d75
David <dhahn123@gmail.com>
2019-04-24 13:21:11 -0700

    Import Schema into Server.js and include schema

--- node-apollo/server.js
+++ node-apollo/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import schema from './schema';
 
 const app = express();
 
@@ -8,6 +9,6 @@ app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql'
 }));
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({}));
+app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
 
 app.listen(4000, () => console.log(`Express server running on port 4000`));
